27d49219b61577eccf386ac36071ad20eaac9046
pradeepbasyal2003 <pradeepbasyal2003@gmail.com>
2024-04-27 21:13:29 +0545

    work on models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Experiences(models.Model):
+    date = models.CharField(max_length=200)
+    position = models.CharField(max_length =200)
+    company = models.CharField(max_length =200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.date
+
+class Contact(models.Model):
+    name = models.CharField(max_length = 300)
+    email = models.EmailField(max_length = 100)
+    message = models.TextField()
+
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
+    views = {}
+    delay = 0
+    views['experiences'] = Experiences.objects.all()
 
-    return render(request , 'index.html')
+    def update_delay(delay):
+        delay=delay + .2
+        return delay
+    return render(request , 'index.html' , views)
 
