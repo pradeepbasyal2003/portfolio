37aa66299487ad3c38cd4f9b464c82cb193ed3ab
pradeepbasyal2003 <pradeepbasyal2003@gmail.com>
2024-05-01 10:36:48 +0545

    contact form

--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class Experiences(models.Model):
     date = models.CharField(max_length=200)
@@ -14,7 +14,7 @@ class Contact(models.Model):
     name = models.CharField(max_length = 300)
     email = models.EmailField(max_length = 100)
     message = models.TextField()
-
+    phone = PhoneNumberField(blank = True)
 
     def __str__(self):
         return self.name
--- home/views.py
+++ home/views.py
@@ -6,6 +6,23 @@ def home(request):
     delay = 0
     views['experiences'] = Experiences.objects.all()
 
+
+
+    # contact post method
+    if request.method =='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        phone = request.POST['phone']
+        data = Contact.objects.create(
+            name = name,
+            email = email,
+            message = message,
+            phone = phone,
+        )
+        data.save()
+
+    # delay for experiences section
     def update_delay(delay):
         delay=delay + .2
         return delay
